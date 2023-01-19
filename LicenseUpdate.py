8f49542b39b8d2d3d01418cca6f04081334756f7
AureliusAtilius <j.j.h.lindner@gmail.com>
2023-01-18 16:53:06 -0800

    Initial commit.

---
+++ LicenseUpdate.py
@@ -0,0 +1,11 @@
+import json
+import paramiko
+
+
+#TODO: create function for reading JSON file with host details
+
+#TODO: Create function for connecting with host
+
+#TODO: Create function for transferring license file
+
+#TODO: Create function for running sudo commands
